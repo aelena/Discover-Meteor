cf79c40ac27536dcdfd6e94fa2440e3be8d8a38a
aelena <antonioelena@gmail.com>
2013-05-01 17:22:58 +0200

    change in the static data source and the view (including a description and showing it)

--- microscope/client/views/posts/posts_list.js
+++ microscope/client/views/posts/posts_list.js
@@ -2,18 +2,26 @@ var postsData = [
 	
 	{
 		title: 'Introducing Telescope',
+		alt: 'Introducing Telescope',
 		author: 'Sacha Greif',
-		url: 'http://sachagreif.com/introducing-telescope/'
+		url: 'http://sachagreif.com/introducing-telescope/',
+		data_desc: 'The authoritative guide to Telescope'
 	},
 	{
 		title: 'Meteor', 
+		alt: 'Meteor', 
 		author: 'Tom Coleman', 
-		url: 'http://meteor.com'
+		url: 'http://meteor.com',
+		data_desc: 'The authoritative and opinionated guide to Meteor'
+
 	}, 
 	{
 		title: 'The Meteor Book', 
+		alt: 'The Meteor Book', 
 		author: 'Tom Coleman',
-		url: 'http://themeteorbook.com'
+		url: 'http://themeteorbook.com',
+		data_desc: 'The überguide to the hottest hipster hacker framework!'
+
 	}
 ];
 
