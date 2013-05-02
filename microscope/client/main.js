e0e6c2c7fdbbc2281081e0ed8eecb390ff116bdb
aelena <antonioelena@gmail.com>
2013-05-03 00:24:43 +0200

    publication added
    autopublish removed

---
+++ microscope/client/main.js
@@ -0,0 +1,5 @@
+/** 
+ * set up the subscription so that the clint
+ * actually gets the data 
+ */
+ Meteor.subscribe('posts');
---
+++ microscope/server/publications.js
@@ -0,0 +1,8 @@
+/**
+ * So far no  paging, or showing just 
+ * specific stuff for specific users
+ * that will come later on
+ */
+Meteor.publish('posts', function(){
+	return Posts.find();
+})
